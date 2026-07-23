9dd2b1e52f10f608f5b34d4ed66228164a8c22d4
Nikki-Martinez <lmartinezj5@miumg.edu.gt>
2026-07-22 21:55:17 -0600

    Se creo estructura principal  del proyecto

---
+++ pdf-studio/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-red-600">PDF Studio Listo</h1>
+    </div>
+  );
+}
